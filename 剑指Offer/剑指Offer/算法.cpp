ecf5b68447abe43aafcfab46db8546181aa6fc46
ChunGang5 <1330431376@qq.com>
2020-07-30 23:02:58 +0800

    数组边界问题和查找

--- 剑指Offer/剑指Offer/算法.cpp
+++ 剑指Offer/剑指Offer/算法.cpp
@@ -86,3 +86,36 @@
 //		return ret[len];
 //	}
 //};
+
+
+/*在一个二维数组中（每个一维数组的长度相同），
+每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+https://www.nowcoder.com/practice/abc3fe2ce8e146608e868a70efebf62e?tpId=13&tqId=11154&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/codinginterviews/question-ranking
+*/
+//class Solution {
+//public:
+//	bool Find(int target, vector<vector<int> > array)
+//	{
+//		if (array.empty())
+//		{
+//			return false;
+//		}
+//		//从左下角或者右上角解决问题。我们选择从右上角解决问题
+//		int row = 0;
+//		int col = array[0].size() - 1;
+//		//边界条件
+//		while (row<array.size() && col >= 0)
+//		{
+//			if (array[row][col]>target)
+//				//array[row][col]是当前行最大的，当前列最小的
+//				--col;
+//			else if (array[row][col]<target)
+//				++row;
+//			else
+//				return true;
+//		}
+//		return false;
+//	}
+//};
+
