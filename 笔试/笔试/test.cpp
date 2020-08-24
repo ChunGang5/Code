938194bfa562b5a94c24edd2ffe43f52dc54a298
ChunGang5 <1330431376@qq.com>
2020-08-24 23:28:12 +0800

    CVTE

--- 笔试/笔试/test.cpp
+++ 笔试/笔试/test.cpp
@@ -146,7 +146,7 @@ using namespace std;
 
 
 
-#include<algorithm>
+//#include<algorithm>
 //int CalulateMethodCount(int num_money)
 //{
 //	if (num_money == 1)
@@ -173,17 +173,195 @@ using namespace std;
 
 
 
-int main()
+//int main()
+//{
+//	int a = 0, b = 0, c = 0;
+//	while (cin >> a >> b >> c)
+//	{
+//		vector<int> result;
+//		result.push_back(a);
+//		result.push_back(b);
+//		result.push_back(c);
+//		sort(result.begin(), result.end());
+//		cout << "[" << result[0] << "," << " " << result[1] << "," << " " << result[2] << "]" << endl;
+//	}
+//	return 0;
+//}
+
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//bool Is_range(string& s)
+//{
+//	bool flag = true;
+//	for (int i = 0; i < s.size(); ++i)
+//	{
+//		if (!(('A' <= s[i]&& s[i]<= 'Z') ||( 'a' <= s[i]&& s[i]<= 'z')))
+//		{
+//			flag = false;
+//			break;
+//		}
+//	}
+//	return flag;
+//}
+//int main()
+//{
+//	int n = 0;
+//	while (cin >> n)
+//	{
+//		int result = 0;
+//		string s;
+//		for (int i = 0; i < n; ++i)
+//		{
+//			cin >> s;
+//			if ((0<=s.size() <= 10) && Is_range(s))
+//			{
+//				++result;
+//			}
+//		}
+//		cout << result << endl;
+//	}
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<vector>
+//#include<list>
+//using namespace std;
+//
+//void Move_End_Num(list<int>& number)
+//{
+//	int num = *number.begin();
+//	number.erase(number.begin());
+//	number.push_back(num);
+//}
+//void Swap(list<int>& number)
+//{
+//	auto it = number.begin();
+//	while (it != number.end())
+//	{
+//		swap(it, ++it);
+//		++it;
+//	}
+//}
+//int main()
+//{
+//	int n, m;
+//	while (cin >> n >> m)
+//	{
+//		if (n % 2!=0)
+//		{
+//			break;
+//		}
+//		vector<int> op(m);
+//		for (int i = 0; i < op.size(); ++i)
+//		{
+//			cin >> op[i];
+//		}
+//		list<int> number;
+//		for(int i = 0; i < number.size(); ++i)
+//		{
+//			number.push_back(i + 1);
+//		}
+//		for (int i = 0; i < op.size(); ++i)
+//		{
+//			if (op[i] == 1)
+//			{
+//				Move_End_Num(number);
+//			}
+//			else
+//			{
+//				Swap(number);
+//			}
+//		}
+//		for (auto it = number.begin(); it != number.end(); ++it)
+//		{
+//			cout << *it;
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+//void Find_Char(string& str)
+//{
+//	int start = 0;
+//	auto pos = 0;
+//	while (pos < str.size())
+//	{
+//		pos = str.find(start+1, 'n');
+//		if (pos != str.npos)
+//		{
+//			str[start] -= 32;
+//			for (int i = start; i < pos; ++i)
+//			{
+//				cout << str[i];
+//			}
+//			cout << endl;
+//		}
+//		start = pos;
+//	}
+//}
+//int main()
+//{
+//	string str;
+//	cin >> str;
+//	if (str.empty())
+//	{
+//		return 0;
+//	}
+//	int start = 0;
+//	auto pos = 0;
+//	while (pos < str.size())
+//	{
+//		pos = str.find( 'n', start + 1);
+//		if (pos != str.npos)
+//		{
+//			str[start] -= 32;
+//			for (int i = start; i < pos; ++i)
+//			{
+//				cout << str[i];
+//			}
+//			cout << endl;
+//			start = pos;
+//		}
+//	}
+//	if (start < str.size())
+//	{
+//		str[start] -= 32;
+//		for (auto i = start; i < str.size(); ++i)
+//		{
+//			cout << str[i];
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+#include<vector>
+int mian()
 {
-	int a = 0, b = 0, c = 0;
-	while (cin >> a >> b >> c)
+	int m, n;
+	cin >> m >> n;
+	vector<vector<int>> dp(m, vector<int>(n));
+	for (int i = 0; i < m; ++i)
 	{
-		vector<int> result;
-		result.push_back(a);
-		result.push_back(b);
-		result.push_back(c);
-		sort(result.begin(), result.end());
-		cout << "[" << result[0] << "," << " " << result[1] << "," << " " << result[2] << "]" << endl;
+		for (int j = 0; j < n; j++)
+		{
+			cin >> dp[i][j];
+		}
 	}
 	return 0;
 }
