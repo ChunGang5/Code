cbb7ee60e0617195b194f964d02f0a07382e15cc
ChunGang5 <1330431376@qq.com>
2020-11-27 22:47:54 +0800

    函数   strConv 类型转换

--- goCode/awesomeProject/test.go
+++ goCode/awesomeProject/test.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main(){
+	/*
 	fmt.Println("test")
+	var num int=10
+	if num<15 {
+		fmt.Println("too letter")
+	}
+
+	var years int
+	fmt.Scanf("%d",&years)
+	if (years%4==0 && years%100!=0)||years%400==0{
+		fmt.Println("闰年")
+	}
+	*/
+
+	var a,b,c float64
+	fmt.Scanf("%f,%f,%f",a,b,c)
+
+	m:=b*b-4*a*c
+
+	if m>0{
+		x1:=(-b+math.Sqrt(m))/2*a
+		x2:=(-b-math.Sqrt(m))/2*a
+		fmt.Printf("x1=%v,x2=%v\n",x1,x2)
+	}else if m == 0{
+		x:=(-b+math.Sqrt(m))/2*a
+		fmt.Printf("x=%v\n",x)
+	}else{
+		fmt.Println("NO")
+	}
+
 }
